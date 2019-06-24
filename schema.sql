d959b0d429670a168a462f497a7eb2387b61503e
Anton Recicalov <Anton.Recicalov@endava.com>
2019-06-24 15:30:27 +0300

    added indexes to columns

--- schema.sql
+++ schema.sql
@@ -8,9 +8,9 @@ DROP TABLE IF EXISTS `currency`;
 CREATE TABLE `currency`
 (
   `currency_id`   bigint(100) NOT NULL,
-  `currency_name` varchar(3)  NOT NULL,
+  `currency_value` varchar(3)  NOT NULL,
   PRIMARY KEY (`currency_id`),
-  UNIQUE KEY `currency_name_UNIQUE` (`currency_name`)
+  UNIQUE KEY `currency_name_UNIQUE` (`currency_value`)
 );
 
 CREATE TABLE `balance`
@@ -19,10 +19,13 @@ CREATE TABLE `balance`
   `amount`      decimal(4, 0) NOT NULL,
   `currency_id` bigint(100)   NOT NULL,
   `user_id`     bigint(100) NOT NULL,
-  `opt_lock`    int(100),
+  `opt_lock`    int(100) DEFAULT 0,
   PRIMARY KEY (balance_id, user_id, currency_id),
   CONSTRAINT `fk_balance_currency` FOREIGN KEY (`currency_id`) REFERENCES `currency` (`currency_id`) ON UPDATE CASCADE
 );
+
+CREATE INDEX user_idx ON balance (user_id) USING BTREE;
+CREATE INDEX cur_idx ON balance (currency_id) USING BTREE;
 
 -- test data
 INSERT INTO `currency`
